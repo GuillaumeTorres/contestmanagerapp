8b817b91ec2585dbae1593585dbdcd574016efaa
Hugo Sudre-Rouffaux <Hugo Sudre-Rouffaux>
2016-12-04 22:22:13 +0100

    Debut form mission

--- src/js/MissionCtrl.js
+++ src/js/MissionCtrl.js
@@ -5,6 +5,16 @@ module.exports = angular.module('myApp.mission', [])
 
 .controller('MissionCtrl', function ($scope, $ionicPopover, $http, $state, $stateParams) {
 
+	var test = $scope.test;
 	console.log('MissionCtrl :)');	
+	console.log('test : ');
+	if(test) console.log($scope.test);
+	else {
+		console.log('blblblbl');
+
+		$scope.test = false;
+
+		console.log($scope.test);
+	}
 
 })
