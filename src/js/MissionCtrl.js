ce1de5b5078174ca8d07eb1d39b432612e90d99f
HugoS-R <hugo.sudre-rouffaux@wister.fr>
2016-12-06 20:09:35 +0100

    BONTON RADIO OUIIIII

--- src/js/MissionCtrl.js
+++ src/js/MissionCtrl.js
@@ -6,6 +6,7 @@ module.exports = angular.module('myApp.mission', [])
 .controller('MissionCtrl', function ($scope, $ionicPopover, $http, $state, $stateParams) {
 
 	var test = $scope.test;
+	var test2 = $scope.test2;
 	console.log('MissionCtrl :)');	
 	console.log('test : ');
 	if(test) console.log($scope.test);
@@ -17,4 +18,13 @@ module.exports = angular.module('myApp.mission', [])
 		console.log($scope.test);
 	}
 
+	console.log('test2 : ');
+	if(test2) console.log($scope.test2);
+	else {
+		console.log('blblblbl');
+
+		$scope.test2 = false;
+
+		console.log($scope.test2);
+	}
 })
