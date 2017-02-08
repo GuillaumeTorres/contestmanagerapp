c2e9cd17923374af96cff0ca39b9fa32ffa176d0
HugoS-R <hugo.sudre-rouffaux@wister.fr>
2017-02-08 18:07:22 +0100

    match en dynamique

--- src/js/MatchCtrl.js
+++ src/js/MatchCtrl.js
@@ -2,44 +2,54 @@
 
 module.exports = angular.module('myApp.match', [])
 
-.controller('MatchCtrl', function ($scope, $ionicPopover, $http) {
+.controller('MatchCtrl', function ($scope, $ionicPopover, $http, myService, GlobalVar) {
+
+  $scope.showLoad = true;
+  $scope.showMatch = false;
 
   $scope.onSwipeLeft = function () {
       console.log('message');
   };
   $scope.lol = true;
 
-  $scope.finished = function (finished){
-    if(finished) return true;
-    else return false;
-  };
-
   var team = JSON.parse(window.localStorage.getItem('team'));
-  var id = team.id;
+  var idTeam = team.id;
   console.log('team');
   console.log(team);
   console.log('id');
-  console.log(id);
+  console.log(idTeam);
+  var urlMatch = 'matchs/team/' + idTeam;
   //window.localStorage.setItem( 'user', JSON.stringify(donnees[i]));
+  searchMatch();
 
-  $http.get("matchs.json")                                            
-  .success(function(data, status, headers, config) {
-      var donnees = data;
-      console.log('donnees');
+  function searchMatch() {
+
+    myService.async(urlMatch).then(function(d) {
+      console.log('Reel donnees match : ');
+      var donnees = d;
+      console.log('donnees : ');
       console.log(donnees);
-      var matchs = [];
-      var i = 0;
-      while(donnees[i] != undefined){
-        if(id == donnees[i].team1.id || id == donnees[i].team2.id) {
-          console.log('Bon match : ');
-          console.log(donnees[i]);
-          matchs.push(donnees[i]);
-          // window.localStorage.setItem( 'user', JSON.stringify(donnees[i]));
-        }
-        i++;
-      }
-      $scope.match = matchs;                       
-   }); 
+
+      angular.forEach(donnees, function(value) {
+        var finished = value.versus.finished;
+
+        if(finished) $scope.matchsFinish = donnees;
+        else $scope.matchsNotFinish = donnees;
+
+      });
+
+
+      $scope.showLoad = false;
+      $scope.rotate = false;
+      $scope.showMatch = true;
+    });
+  }
+
+  $scope.searchAgain = function() {
+    $scope.rotate = true;
+    searchMatch();
+  }
+
 
 
 
@@ -52,12 +62,12 @@ module.exports = angular.module('myApp.match', [])
   // }).then(function successCallback(response) {
   //     // this callback will be called asynchronously
   //     // when the response is available
-  //     // 
+  //     //
   //     console.log(response.data);
   //     $scope.match = response.data;
   //   }, function errorCallback(response) {
   //     // called asynchronously if an error occurs
   //     // or server returns response with an error status.
   //     console.log('TrucMachin');
-  //   }); 
+  //   });
 })
